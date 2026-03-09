1ee9e2bfcb07abd9c3086ce7adcbb682293e4847
Davide Lescio <davidelescio@MacBook-Air-di-Davide.local>
2026-03-09 20:07:30 +0100

    Fix

--- app.py
+++ app.py
@@ -383,7 +383,7 @@ socketio = SocketIO(
         "http://127.0.0.1:5050",
         "http://localhost:5050"
     ],
-    client_manager=RedisManager(redis_url),
+    message_queue=redis_url,
     ping_timeout=20,
     ping_interval=10,
     logger=True,
